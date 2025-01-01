0b8828101200b54031136d7d545a22b2d27a7c6a
Brandon <kaiutoshan@gmail.com>
2025-01-01 10:01:04 -0800

    Initial Commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Header from "../Header/Header.jsx";
+import Main from "../Main/Main.jsx";
+// import "./Footer.jsx";
+
+function App() {
+  return <div className="app"></div>;
+}
+
+export default App;
