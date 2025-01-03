c13d4f0ebc9c88939dea4a34ca7a999b5e4cf4d5
BigRedCoding <brandondooleycoding@gmail.com>
2025-01-03 14:41:03 -0800

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__detail">Developed by Brandon Dooley</p>
+      <p className="footer__detail">2025</p>
+    </div>
+  );
+}
+
+export default Footer;
